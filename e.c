e8cca102268903e7221de67e4599fc8deadb5fa3
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-02-11 15:02:31 -0300

    Update e.c

--- e.c
+++ e.c
@@ -1,1 +1,8 @@
-
+int main(void){
+    int evento = 5 ;
+    char corrida = ’C’;
+    float tempo = 27.25;
+    printf("O tempo vitorioso na eliminatória %c",corrida);
+    printf("\nda competi¸c~ao %d foi %f.", evento, tempo);
+    return 1;
+} 
