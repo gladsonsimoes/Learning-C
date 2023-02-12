748b849f7c98a4d05e21fe4575a7779b70a73eb5
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-12 14:52:44 -0300

    add Scanf

---
+++ Projects_C/beginner_code/src/07_Scanf/Scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int A, B;
+
+   printf("\nDigite o valor 1: ");
+   scanf("%d" , A );
+
+   printf("\nDigite o valor 2:");
+   scanf("%d" , B);
+
+   system("pause");
+
+
+return 0;
+}
