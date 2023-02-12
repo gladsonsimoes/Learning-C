2f9f5bacea07dc7e5412974469608e7c9b98f0b6
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-11 23:01:14 -0300

    Add Basic Project

---
+++ Projects_C/beginner_code/Test/src/01_HelloWorld/HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("Hello World!");
+
+    system("pause"); //somente para windows
+    return 0;
+}
---
+++ Projects_C/beginner_code/Test/src/02_PrintName/PrintName.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    printf("\n My name is Gladson Silva Simoes\n");
+
+    system("pause"); //somente para windows
+    return 0;
+}
