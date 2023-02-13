94c7a2bd05107bd5827baebaf82b51084fd690dc
gladsonsimoes <gladsonssimoes@outlook.com.br>
2023-02-13 09:55:36 -0300

    Adicionei um arquivo sobre vetor

---
+++ Content/src/06_Vector/Vector.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int A=5 , i , vet[5];
+    vet[0] = 1;
+    vet[1] = 2;
+
+    printf("\n vetor: %d \n", vet[0]);
+
+    system("pause");
+    return 0;
+}
