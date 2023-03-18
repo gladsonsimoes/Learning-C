f9bc27fee664398437b387062004b2f7dd15ad3b
Gladson Simões <gladsonssimoes@outlook.com.br>
2023-03-18 13:43:23 -0300

    Update DeclareString.c

--- Content/src/08_DeclareString/DeclareString.c
+++ Content/src/08_DeclareString/DeclareString.c
@@ -3,18 +3,6 @@
 
 void main()
 {
-    //String que ignora espaço \/
-
-    // char nome[200];
-
-    // printf("Digite seu nome: ");
-    // scanf("%s" , nome); //o ruim é que depois do espaço ele ignora os caracteres
-
-    // printf("Seu Nome: %s \n" , nome); //ele não printa o as letras depois do espaço pois ele ignora
-
-
-    //String que não ignora o espaço: \/
-
     char nomeCompleto[100];
 
     printf("Digite seu nome completo: ");
